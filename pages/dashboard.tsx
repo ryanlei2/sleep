4a6e518761506e5d2992bbe33af22847bb2f6aa7
Ryan Lei <ryanschool@ryans-MacBook-Air.local>
2023-01-09 14:46:13 -0800

    Revert "Revert "build app and change icon""
    This reverts commit cdb02ca2146ade85654c7fb689675914f33c7272.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { auth } from '../config/firebase';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import Link from 'next/link';
 const Dashboard = () => {
   return (
     <div className="bodyText">
       <Container className="mx-auto">
         <i><b>First</b></i> time using Coursee or having trouble deciding on a path? Take our survey!<br></br>
-        <a href="/survey">
+        <Link href="/survey">
+          <a>
           <button className="surveyBtn" role="button">Survey</button>
-        </a>
+          </a>
+        </Link>
+        
       </Container>
     </div>
       
