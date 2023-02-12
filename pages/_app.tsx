2286396581d69328fd1d3924040fb7cf6ab9725a
Ryan Lei <ryanlei726@gmail.com>
2023-02-12 03:01:52 -0800

    finished index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,8 @@ import Navbar from '../components/Navbar'
 import { AuthContextProvider } from '../context/AuthContext'
 import { useRouter } from 'next/router'
 import ProtectedRoute from '../components/ProtectedRoute'
-import { Inter } from '@next/font/google'
 
 const noAuthRequired = ['/', '/about', '/login', '/signup', '/faq'] //THIS IS WHERE YOU WHITELIST PAGES
-
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,54 @@
 import type { NextPage } from 'next'
-import { Button, Card, Container } from 'react-bootstrap'
+import { Button, Card, Container, Row, Col} from 'react-bootstrap'
 import styles from '../styles/home.module.css'
 import banner from '../assets/banner.png'
 import Link from 'next/link'
 import { IBM_Plex_Sans } from '@next/font/google'
 import { useAuth } from '../context/AuthContext'
 import { useRouter } from 'next/router'
-
+import Image from 'next/image'
 
 const Home: NextPage = () => {
+  function handleClick() {
+    console.log('increment like count');
+  }
+  const { user, logout } = useAuth()
+  const router = useRouter()
+
+  try {
+    // Your code to display the image
+  } catch (error) {
+    console.error(error);
+  }
   return (
     <div>
-      
-      {/* <div className='' >
-        <Image 
-              src={banner}
-              alt='Brand logo'
-              fill
-            />
-      </div> */}
-        
-        <div className={styles.banner}>
-          <b className={styles.bannerTitle}>Let&apos;s Plan Together</b>
-          <br></br>
-          <p className={styles.bannerSubtext}>We&apos;re on a mission to help every student choose the right path.</p>
-          
-            <Link href={"/signup"}
-            className={styles.getStartedBtn} 
-            legacyBehavior>
-              <a className={styles.bannerButton}
-                onClick={() => {
-                  {user ? (
-                    router.push('/survey')
-                  ) : (
-                    router.push('/signup')
-                  )}
-                    
-                  }} 
-              >Get Started</a>
-              {/* 
-                test
-              */}
-            </Link>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "500px", width:'100vw' }}>
+        <Container fluid style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%", width: "100vw" }}>
+          <div style={{ position: "relative", height: "100%", width: "100vw" }}>
+            <Image src={banner} alt='banner' style={{ height: "100%", width: "100vw", objectFit: "cover",  }} />
+            <div style={{ position: "absolute", top: 0, left: 0, height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+              <h1 style={{ fontSize: "2em" }}>
+                <b style={{ marginTop:'30px', fontSize: "7vw" }}>Let&apos;s Plan Together</b>
+                <br></br>
+                <p style={{ fontSize: "2vw" }}>We&apos;re on a mission to help every student choose the right path.</p>
+                <Link href={"/signup"}
+                legacyBehavior>
+                  <a style={{ fontSize: "30px", marginTop:'0'}}
+                    onClick={() => {
+                      {user ? (
+                        router.push('/survey')
+                      ) : (
+                        router.push('/signup')
+                      )}
+                        
+                      }} 
+                  >Get Started</a>
+                </Link>
+              </h1>
+            </div>
+          </div>
+        </Container>
       </div>
-      
       <div
       style={{
         width: '40%',
