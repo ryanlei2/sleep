56e48baeafa3b7f85eedab7fe77bec6cbc22f0b0
Ryan Lei <ryanlei726@gmail.com>
2023-02-22 01:41:33 -0800

    created administrator priviledges

---
+++ components/AdminDashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminDashboard() {
+  return (
+    <div>
+        <h1 className='display-3'>AdminDashboard</h1>
+        {/* display survey results and whether or not students approved these, or gave feedback on em */}
+    </div>
+  )
+}
