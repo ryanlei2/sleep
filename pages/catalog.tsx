ffe3e2a9a86707c492a2c64dd009fb8dc26c9494
ryanlei2 <ryanlei726@gmail.com>
2023-03-22 21:50:09 -0700

    please work

--- pages/catalog.tsx
+++ pages/catalog.tsx
@@ -6,11 +6,11 @@ const Catalog = () => {
   return (
     <Container className='display-1'
     style={{
-      marginTop:'3rem',
+      marginTop:'150px',
       textAlign:'center'
     }}
     >
-      <Link target="_blank" href='https://resources.finalsite.net/images/v1653403002/rsdedu/v8gyhn1oxq83dtsqsw46/HS2022-23Catalogv6.pdf'>Catalog Page</Link>
+      <Link target="_blank" href='https://resources.finalsite.net/images/v1677715498/rsdedu/livi36kif2azcpamtafa/RSDHighSchoolCatalog2023-2024Digital.pdf'>Catalog Page</Link>
     </Container>
   );
 };
